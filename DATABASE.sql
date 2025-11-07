a55f45d74e136e7827adf21415298345c26a13a6
Rohit Lakare <108202946+rlakare5@users.noreply.github.com>
2025-11-07 12:25:38 +0530

    Add files via upload

---
+++ DATABASE.sql
@@ -0,0 +1,30 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: May 31, 2022 at 11:06 PM
+-- Server version: 10.4.22-MariaDB
+-- PHP Version: 8.1.2
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: ``
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table ``
+--
+
+CREATE TABLE 
